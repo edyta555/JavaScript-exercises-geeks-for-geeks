6a4625a70d66417b296182b57893223dca65c0ce
Edyta Badowska <edyta.badowska555@gmail.com>
2022-10-20 21:22:46 +0200

    fix: sort an array of 0s, 1s and 2s using counting

--- sorting/dutch-national-flag-problem.js
+++ sorting/dutch-national-flag-problem.js
@@ -16,16 +16,15 @@
 
 /* First approach: using the pointer
 
-Solution:
+  Solution:
     - Keep three indices low = 1, mid = 1, and high = N and there are four ranges, 1 to low (the range containing 0), low to mid (the range containing 1), mid to high (the range containing unknown elements) and high to N (the range containing 2).
     - Traverse the array from start to end and mid is less than high. (Loop counter is i)
     - If the element is 0 then swap the element with the element at index low and update low = low + 1 and mid = mid + 1
     - If the element is 1 then update mid = mid + 1
     - If the element is 2 then swap the element with the element at index high and update high = high – 1 and update i = i – 1. As the swapped element is not processed
 
-    Time Complexity: O(n), Only one traversal of the array is needed.
-    Space Complexity: O(1), No extra space is required.
-
+  Time Complexity: O(n), Only one traversal of the array is needed.
+  Space Complexity: O(1), No extra space is required.
 */
 
 function sortArrayUsingPointer(sampleArray) {
@@ -55,5 +54,58 @@ function sortArrayUsingPointer(sampleArray) {
 const firstExample = sortArrayUsingPointer([0, 1, 2, 0, 1, 2]);
 console.log("first example:", firstExample);
 
-const secondExample = sortArrayUsingPointer([0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1]);
+const secondExample = sortArrayUsingPointer([
+  0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1,
+]);
 console.log("second example", secondExample);
+
+/* Second approach: using counting
+
+  Solution:
+    - Keep three counters c0 to count 0s, c1 to count 1s, and c2 to count 2s
+    - Traverse through the array and increase the count of c0 if the element is 0, increase the count of c1 if the element is 1 and increase the count of c2 if the element is 2
+    - Now again traverse the array and replace the first c0 elements with 0, the next c1 elements with 1, and the next c2 elements with 2.  
+    
+  Time Complexity: O(n)
+  Space Complexity: O(1)
+*/
+
+function sortArrayUsingCounting(sampleArray) {
+  let counter0 = 0,
+    counter1 = 0,
+    counter2 = 0;
+  // count the number of 0s, 1s and 2s in the array
+  sampleArray.forEach((value) => {
+    if (value === 0) {
+      counter0++;
+    } else if (value === 1) {
+      counter1++;
+    } else if (value === 2) {
+      counter2++;
+    }
+
+    //update the array
+    let currectIndex = 0;
+    while (counter0 > 0) {
+      sampleArray[currectIndex++] = 0;
+      counter0--;
+    }
+    while (counter1 > 0) {
+      sampleArray[currectIndex++] = 1;
+      counter1--;
+    }
+    while (counter2 > 0) {
+      sampleArray[currectIndex++] = 2;
+      counter2--;
+    }
+  });
+  return sampleArray;
+}
+
+const thirdExample = sortArrayUsingPointer([0, 1, 2, 0, 1, 2]);
+console.log("third example:", thirdExample);
+
+const fourthExample = sortArrayUsingPointer([
+  0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1,
+]);
+console.log("fourth example", fourthExample);
