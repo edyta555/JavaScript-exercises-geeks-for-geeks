215f670d0253cdd8ed80fa0c95f9e8e91e16c03c
Edyta Badowska <edyta.badowska555@gmail.com>
2022-10-22 17:28:40 +0200

    fix: sort an array in wave form (optimized approach)

--- sorting/sort-array-in-wave-form.js
+++ sorting/sort-array-in-wave-form.js
@@ -1,12 +1,5 @@
 /*  Given an unsorted array of integers, sort the array into a wave array. An array arr[0..n-1] is sorted in wave form if:
     arr[0] >= arr[1] <= arr[2] >= arr[3] <= arr[4] >= …..
-
-    Examples:
-        Input:  arr[] = {10, 5, 6, 3, 2, 20, 100, 80}
-        Output: arr[] = {10, 5, 6, 2, 20, 3, 100, 80} 
-
-        Input: arr[] = {20, 10, 8, 6, 4, 2}
-        Output: arr[] = {20, 8, 10, 4, 6, 2}
 */
 
 /*  First approach: using sorting
@@ -25,7 +18,7 @@ function swapTwoElements(sampleArray, firstIndex, secondIndex) {
   sampleArray[secondIndex] = replacedValue;
 }
 
-function waveArray(sampleArray) {
+function sortArrayInWave(sampleArray) {
   sampleArray.sort((a, b) => a - b);
   for (let i = 0; i < sampleArray.length - 1; i += 2) {
     swapTwoElements(sampleArray, i, i + 1);
@@ -33,8 +26,53 @@ function waveArray(sampleArray) {
   return sampleArray;
 }
 
-const firstExample = [10, 90, 49, 2, 1, 5, 23];
-console.log("first example: ", waveArray(firstExample));
+const firstExample = sortArrayInWave([10, 5, 6, 3, 2, 20, 100]);
+const secondExample = sortArrayInWave([20, 10, 8, 6, 4, 2]);
 
-const secondExample = [20, 10, 8, 6, 4, 2];
-console.log("second example", waveArray(secondExample));
+console.log("first example: ", firstExample);
+console.log("second example", secondExample);
+
+/* optimized approach
+  The idea is based on the fact that if we make sure that all even positioned (at index 0, 2, 4, ..) 
+  elements are greater than their adjacent odd elements, we don’t need to worry about oddly positioned elements. 
+
+  Examples:
+    Input:  arr[] = {10, 5, 6, 3, 2, 20, 100}
+    Output: arr[] = {10, 5, 6, 2, 20, 3, 100} 
+
+    Input: arr[] = {20, 10, 8, 6, 4, 2}
+    Output: arr[] = {20, 8, 10, 4, 6, 2}
+
+  Solution:
+  - Traverse all even positioned elements of the input array, and do the following. 
+    - If the current element is smaller than the previous odd element, swap the previous and current. 
+    - If the current element is smaller than the next odd element, swap next and current.
+
+  Time Complexity: O(N)
+  Auxiliary Space: O(1)
+*/
+
+function sortArrayInWave2(sampleArray) {
+  const arrayLength = sampleArray.length;
+  for (let i = 0; i < arrayLength; i += 2) {
+    if (i > 0 && sampleArray[i] < sampleArray[i - 1]) {
+      [sampleArray[i - 1], sampleArray[i]] = [
+        sampleArray[i],
+        sampleArray[i - 1],
+      ];
+    }
+
+    if (i < arrayLength && sampleArray[i] < sampleArray[i + 1]) {
+      [sampleArray[i], sampleArray[i + 1]] = [
+        sampleArray[i + 1],
+        sampleArray[i],
+      ];
+    }
+  }
+  return sampleArray;
+}
+
+const thirdExample = sortArrayInWave2([10, 5, 6, 3, 2, 20, 100]);
+console.log("third example:", thirdExample);
+const fourthExample = sortArrayInWave2([20, 10, 8, 6, 4, 2]);
+console.log("fourth example:", fourthExample);
